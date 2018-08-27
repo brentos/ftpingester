820b77ed02bfb4365344c9e136619cbabd8ce00f
Brent Yarger <byarger@redhat.com>
2018-08-27 15:53:52 -0700

    Add logging example

--- src/main/java/com/example/inc/ftpingester/FtpRoute.java
+++ src/main/java/com/example/inc/ftpingester/FtpRoute.java
@@ -1,9 +1,6 @@
 package com.example.inc.ftpingester;
 
-import java.io.File;
-
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -15,15 +12,8 @@ public class FtpRoute extends RouteBuilder {
 
 		// &localWorkDirectory=target/tmp
 		from("ftp://localhost:2221/test?username=admin&password=admin&move=.done&moveFailed=.error&readLock=rename&localWorkDirectory=target/tmp")
-			.convertBodyTo(File.class).process(new Processor() {
-
-				@Override
-				public void process(Exchange exchange) throws Exception {
-					File file = (File)exchange.getIn().getBody();
-					System.out.println(file.getName());
-				}
-				
-			}).to("bean:oprFileProcessor");
+			.log(LoggingLevel.INFO, "Processing file ${file:name}")
+			.to("bean:oprFileProcessor");
 		
 	}
 
