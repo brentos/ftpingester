e10884abd6377a48c98fd849842bc860ae7e471d
Brent Yarger <byarger@redhat.com>
2018-08-27 15:23:08 -0700

    Initial working example

--- src/main/java/com/example/inc/ftpingester/FtpRoute.java
+++ src/main/java/com/example/inc/ftpingester/FtpRoute.java
@@ -1,5 +1,9 @@
 package com.example.inc.ftpingester;
 
+import java.io.File;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +13,26 @@ public class FtpRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
-		from("ftp://");
+		// &localWorkDirectory=target/tmp
+		from("ftp://localhost:2221/test?username=admin&password=admin&move=.done&moveFailed=.error&readLock=rename&localWorkDirectory=target/tmp")
+			.convertBodyTo(File.class).process(new Processor() {
+
+				@Override
+				public void process(Exchange exchange) throws Exception {
+					System.out.println("X");
+					System.out.println("X");
+					System.out.println("X");
+					System.out.println("X");
+					System.out.println("X");
+					System.out.println("X");
+					System.out.println("X");
+					System.out.println("X");
+					System.out.println("X");
+					File file = (File)exchange.getIn().getBody();
+					System.out.println(file.getName());
+				}
+				
+			}).to("file://target");
 		
 	}
 
--- src/main/java/com/example/inc/ftpingester/FtpingesterApplication.java
+++ src/main/java/com/example/inc/ftpingester/FtpingesterApplication.java
@@ -1,8 +1,13 @@
 package com.example.inc.ftpingester;
 
+import java.io.File;
+
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
 import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.ClearTextPasswordEncryptor;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +17,15 @@ public class FtpingesterApplication {
 	public static void main(String[] args) throws FtpException {
 		SpringApplication.run(FtpingesterApplication.class, args);
 		
+		// This is ugly and only being used as an example
 		FtpServerFactory serverFactory = new FtpServerFactory();
+		ListenerFactory listenerFactory = new ListenerFactory();
+		listenerFactory.setPort(2221);
+		PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+		userManagerFactory.setFile(new File("users.properties"));
+		userManagerFactory.setPasswordEncryptor(new ClearTextPasswordEncryptor());
+		serverFactory.setUserManager(userManagerFactory.createUserManager());
+		serverFactory.addListener("default", listenerFactory.createListener());
 		FtpServer server = serverFactory.createServer();
 		server.start();
 		
